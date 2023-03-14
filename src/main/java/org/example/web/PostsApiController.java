5c8440d1e54fe161864dc6d71d44344f8bba3fb4
parkcoldroad <coldstreet@naver.com>
2023-03-14 22:43:10 +0900

    feat: Add update function in PostApiController

--- src/main/java/org/example/web/PostsApiController.java
+++ src/main/java/org/example/web/PostsApiController.java
@@ -3,7 +3,9 @@ package org.example.web;
 import lombok.RequiredArgsConstructor;
 import org.example.service.posts.PostsService;
 import org.example.web.dto.PostsSaveRequestDto;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +19,10 @@ public class PostsApiController {
   public Long save(@RequestBody PostsSaveRequestDto requestDto){
     return postsService.save(requestDto);
   }
+
+  @PutMapping
+  public Long update(@PathVariable Long id, @RequestBody postsUpdateRequestDto request Dto){
+    return postsService.update(id,requestDto);
+  }
+
 }
