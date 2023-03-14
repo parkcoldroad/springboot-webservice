198813a4fd4b6ec819f788e3c7d930b9a05eab81
parkcoldroad <coldstreet@naver.com>
2023-03-14 23:28:00 +0900

    feat: Add update function in PostsService

--- src/main/java/org/example/service/posts/PostsService.java
+++ src/main/java/org/example/service/posts/PostsService.java
@@ -3,7 +3,9 @@ package org.example.service.posts;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.posts.PostRepository;
+import org.example.domain.posts.Posts;
 import org.example.web.dto.PostsSaveRequestDto;
+import org.example.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,5 +18,12 @@ public class PostsService {
   @Transactional
   public Long save(PostsSaveRequestDto requestDto) {
     return postRepository.save(requestDto.toEntity()).getId();
+  }
+
+  public Long update(Long id, PostsUpdateRequestDto requestDto){
+    Posts posts = postRepository.findById(id).orElseThrow(
+        ()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    posts.update(requestDto.getTitle(),requestDto.getContent());
+    return id;
   }
 }
