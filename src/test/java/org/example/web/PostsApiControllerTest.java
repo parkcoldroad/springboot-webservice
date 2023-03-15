b0bdcb3f51344621e2da3c1387bd31ee1e8883c7
parkcoldroad <coldstreet@naver.com>
2023-03-15 19:54:29 +0900

    test: Add 메인페이지 로딩 method to test View

--- src/test/java/org/example/web/PostsApiControllerTest.java
+++ src/test/java/org/example/web/PostsApiControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
