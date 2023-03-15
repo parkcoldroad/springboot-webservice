31a917895041f4428a5e0cbd6c1783a6e9eac97c
parkcoldroad <coldstreet@naver.com>
2023-03-15 22:35:12 +0900

    feat: Add postsUpdate function in IndexController

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -2,9 +2,11 @@ package org.example.web;
 
 import lombok.RequiredArgsConstructor;
 import org.example.service.posts.PostsService;
+import org.example.web.dto.PostResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
   @GetMapping("/posts/save")
   public String postsSave() {
     return "posts-save";
+  }
+
+  @GetMapping("/posts/update/{id}")
+  public String postsUpdate(@PathVariable Long id,Model model){
+    PostResponseDto dto = postsService.findById(id);
+    model.addAttribute("post",dto);
+    return "posts-update";
   }
 }
