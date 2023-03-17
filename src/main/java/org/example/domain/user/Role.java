0f42f01c2954f4f7dae6c71368ce51aafcb77c14
parkcoldroad <coldstreet@naver.com>
2023-03-17 11:17:02 +0900

    feat: Add User domain

---
+++ src/main/java/org/example/domain/user/Role.java
@@ -0,0 +1,2 @@
+package org.example.domain.user;public enum Role {
+}
---
+++ src/main/java/org/example/domain/user/User.java
@@ -0,0 +1,42 @@
+package org.example.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
+
+public class User {
+  @Id
+  @GeneratedValue(Strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private String name;
+  @Column(nullable = false)
+  private String email;
+  @Column
+  private String picture;
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false )
+  private Role role;
+
+  @Builder
+  public User(String name,String email,String picture,Role role){
+    this.name= name;
+    this.email=email;
+    this.picture = picture;
+    this.role = role;
+  }
+
+  public User Update(String name,String picture){
+    this.name = name;
+    this.picture = picture;
+    return this;
+  }
+  public String getRoleKey(){
+    return this.role.getKey();
+  }
+}
