615f05fb1e1bf2fd963be4284b6ca4d0041d673b
parkcoldroad <coldstreet@naver.com>
2023-03-19 16:27:37 +0900

    feat: Add ofNaver function in OAuth

--- src/main/java/org/example/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/example/config/auth/dto/OAuthAttributes.java
@@ -16,7 +16,8 @@ public class OAuthAttributes {
   private String picture;
 
   @Builder
-  public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+  public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name,
+      String email, String picture) {
     this.attributes = attributes;
     this.nameAttributeKey = nameAttributeKey;
     this.name = name;
@@ -26,11 +27,26 @@ public class OAuthAttributes {
 
   public static OAuthAttributes of(String registerationId,
       String userNameAttributeName, Map<String, Object> attributes) {
+    if ("naver".equals(registerationId)) {
+      return ofNaver("id", attributes);
+    }
     return ofGoogle(userNameAttributeName, attributes);
   }
 
-  private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
-    User user = new User();
+  private static OAuthAttributes ofNaver(String userNameAttributeName,
+      Map<String, Object> attributes) {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+    return OAuthAttributes.builder()
+        .name((String) response.get("name"))
+        .email((String) response.get("email"))
+        .picture((String) response.get("profile_image"))
+        .attributes(response)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
+
+  private static OAuthAttributes ofGoogle(String userNameAttributeName,
+      Map<String, Object> attributes) {
     return OAuthAttributes.builder()
         .name((String) attributes.get("name"))
         .email((String) attributes.get("email"))
@@ -38,9 +54,6 @@ public class OAuthAttributes {
         .attributes(attributes)
         .nameAttributeKey(userNameAttributeName)
         .build();
-
-
-
   }
 
   public User toEntity() {
