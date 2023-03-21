2cc2f27a7796c4cde7fdcceb03797196be657baf
parkcoldroad <coldstreet@naver.com>
2023-03-21 12:50:36 +0900

    test: Modify 메인페이지 로딩 method

--- src/main/java/org/example/web/dto/PostsListResponseDto.java
+++ src/main/java/org/example/web/dto/PostsListResponseDto.java
@@ -6,15 +6,16 @@ import org.example.domain.posts.Posts;
 
 @Getter
 public class PostsListResponseDto {
-private Long id;
-private String title;
-private String author;
-private LocalDateTime modifiedDate;
 
-public PostsListResponseDto(Posts entity){
-  this.id = entity.getId();
-  this.title = entity.getTitle();
-  this.author = entity.getAuthor();
-  this.modifiedDate = entity.getModifiedDate();
-}
+  private Long id;
+  private String title;
+  private String author;
+  private LocalDateTime modifiedDate;
+
+  public PostsListResponseDto(Posts entity) {
+    this.id = entity.getId();
+    this.title = entity.getTitle();
+    this.author = entity.getAuthor();
+    this.modifiedDate = entity.getModifiedDate();
+  }
 }
